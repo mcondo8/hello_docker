4b157f48054f4b4d8871c2c0f9d8eb231182b125
Michael R. Condon <mcondo8@wgu.edu>
2021-04-19 23:01:52 -0400

    Moved requirements to root

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)`
+
+
+@app.route("/")
+def index():
+  return """
+  <h1>Hello Docker</h1>
+  <p>A relatively braindead way to get python/Flask running in Docker</p>
+  """
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
